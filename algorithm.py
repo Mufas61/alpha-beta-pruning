ac75f0fdcdcb2849d4e803d928ccd1ed853bd5ae
Lion Pierau <lion.pierau@gmail.com>
2018-05-08 17:43:37 +0200

    added build process for viz

--- algorithm.py
+++ algorithm.py
@@ -8,7 +8,7 @@ import logging
 
 
 def alphabeta(graph, node, isMaximizingPlayer, alpha, beta, out):
-    """alpha-beta-pruning that returns a describtion of done """
+    """alpha-beta-pruning that returns a description of done operations"""
 
     if type(graph[node]) is not list:
         return graph[node]  # value of the node
@@ -22,7 +22,7 @@ def alphabeta(graph, node, isMaximizingPlayer, alpha, beta, out):
             out[child] = {"value": bestVal, "alpha": alpha, "beta": beta}
             if beta <= alpha:
                 break  # cut(graph, child)  # actually break but we need to ????
-        return bestVal
+        return out
 
     else:
         bestVal = float("inf")
@@ -33,18 +33,51 @@ def alphabeta(graph, node, isMaximizingPlayer, alpha, beta, out):
             out[child] = {"value": bestVal, "alpha": alpha, "beta": beta}
             if beta <= alpha:
                 break  # (graph, child)  # actually break but we need to ????
-        return bestVal
+        return out
+
+
+def build(edges, desc):
+    """(edges as dict, layout, isMaximizer) -> graph for visualization"""
+    viz_graph = gv.Digraph(format='png')
+    _build(viz_graph, edges, desc, 0, True)
+    return viz_graph
+
+
+def _build(viz_graph, edges, desc, depth, isMaximizer):
+    for i, node in enumerate(edges):
+
+        # style current node
+        viz_graph.attr('node', shape=('triangle' if isMaximizer else 'invtriangle'))
+
+        # has been processed?
+        bestValue = ""
+        if node in desc:
+            bestValue = desc[node]["value"]
+            alpha = desc[node]["alpha"]  # todo
+            beta = desc[node]["beta"]
+
+        curr_name = str(depth) + "" + str(i)
+        viz_graph.node(curr_name, label=str(bestValue))
+
+        # viz edges
+        if type(edges[node]) is list:
+            for j in edges[node]:
+                next_name = str(depth) + "" + str(j)
+                viz_graph.edge(curr_name, next_name)
+
+        _build(viz_graph, edges[node], desc, depth + 1, not isMaximizer)
 
 
 def test():
-    graph = {
+    edges = {
         'A': ['B', 'C'],
         'B': 1,
         'C': 2,
     }
 
-    f = alphabeta(graph, next(iter(graph)), 0, True, float("-inf"), float("inf"))
-    print(f)
+    desc = alphabeta(edges, next(iter(edges)), True, float("-inf"), float("inf"), {})
+    viz_graph = build(edges, desc)
+    print(viz_graph)
 
 
 if __name__ == '__main__':
